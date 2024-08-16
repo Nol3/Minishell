20813e91522c2a4280435450d5d850c75409011c
Alejandro Cardenas Parejo <alcarden@c3r5s3.42malaga.com>
2024-08-16 21:30:11 +0200

    Daily sync Fri 16 Aug 2024 09:30:11 PM CEST

---
+++ src/signals/signals.c
@@ -0,0 +1,14 @@
+// static void ft_ctrl_c(int sig)
+// {
+// 	if (data)
+// 	(void)sig;
+// 	write(1, "\n", 1);
+// 	rl_on_new_line();
+// 	rl_replace_line("", 0);
+// 	rl_redisplay();
+// }
+
+// ft_signal(void)
+// {
+
+// }
