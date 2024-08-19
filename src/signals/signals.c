9f28de7b4f46ca1bd6f5326d67ca7e4d6c28a96e
Nol3 <alejandrocp_952@hotmail.com>
2024-08-19 19:38:03 +0200

    Daily sync lun 19 ago 2024 19:38:03 CEST

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -1,14 +1,21 @@
-// static void ft_ctrl_c(int sig)
-// {
-// 	if (data)
-// 	(void)sig;
-// 	write(1, "\n", 1);
-// 	rl_on_new_line();
-// 	rl_replace_line("", 0);
-// 	rl_redisplay();
-// }
+#include "../../include/minishell.h"
 
-// ft_signal(void)
-// {
+static void	ctr_c(int signal);
 
-// }
+void	ft_signals(void)
+{
+	signal(SIGINT, ctr_c);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+static void	ctr_c(int signal)
+{
+	(void)signal;
+	if (!g_pack)
+		ioctl(0, TIOCSTI, "\n");
+	else
+		write(1, "\n", 1);
+	rl_replace_line("", 1);
+	rl_on_new_line();
+}
